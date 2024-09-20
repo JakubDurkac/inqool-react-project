b280db81d40ee52361f895eb78790a9ab98211aa
Jakub Durkáč <xdurkac1@fi.muni.cz>
2024-09-20 20:44:43 +0200

    Add React router dom. Add pages Home (index page), Users, Animals, NoPage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Users from "./pages/Users";
+import Animals from "./pages/Animals";
+import NoPage from "./pages/NoPage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/animals" element={<Animals />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
