22bb7a1467a84d9757b8823c97745afc3307a704
Jakub Durkáč <xdurkac1@fi.muni.cz>
2024-09-23 21:28:42 +0200

    Add styles to Home and NoPage page components. Add links to available databases.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home-container">
+      <div className="home-header">Choose database</div>
+      <div className="home-links">
+        <Link to="/users">
+          <button className="btn btn-outline-warning">Users</button>
+        </Link>
+        <Link to="/animals">
+          <button className="btn btn-outline-warning">Animals</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 const NoPage = () => {
-  return <h1>Error 404: Page not found</h1>;
+  return (
+    <div className="no-page-container">
+      <div className="no-page-header">Error 404: Page not found</div>
+      <Link to="/">
+        <button className="btn btn-outline-warning">Choose database</button>
+      </Link>
+    </div>
+  );
 };
 
 export default NoPage;
